5f66b6fd4954b8708eb35fe63645a44d452592cd
Yaman <yamanniroula11@gmail.com>
2019-10-22 11:37:37 +0530

    UNIT TESTING

--- src/main/java/com/example/testing/Tester/Service/BookService.java
+++ src/main/java/com/example/testing/Tester/Service/BookService.java
@@ -3,6 +3,8 @@ package com.example.testing.Tester.Service;
 import com.example.testing.Tester.Entity.Book;
 import com.example.testing.Tester.Repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -24,10 +26,19 @@ public class BookService {
 
     }
 
-    public String add(Book book) {
-        bookRepository.save(book);
-        return "ADDED SUCCESSFULLY";
+    public ResponseEntity<List<Book>> add(Book book) {
 
+        if(book.getTitle().length()>=5){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        bookRepository.save(book);
+        List<Book> al = new ArrayList<>();
+        for (Book b : bookRepository.findAll()) {
+            al.add(b);
+        }
+
+
+        return new ResponseEntity<>(al,HttpStatus.OK);
     }
 
     public Book find(int id) {
